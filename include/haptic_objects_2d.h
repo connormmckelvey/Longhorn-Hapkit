d7185effe5d571d279c71a4d458cfc2db5d06ba5
Connor McKelvey <connormmckelvey@gmail.com>
2026-03-04 16:29:28 -0600

    write here

--- include/haptic_objects_2d.h
+++ include/haptic_objects_2d.h
@@ -1,1 +1,28 @@
 //use this file to define the haptic objects in 2D space
+
+//used for representing position or velocity of the end effector in 2D space, as well as forces to apply in 2D space
+typedef struct Vector2{
+    float x;
+    float y;
+} vector2_t;
+
+//a point in 2d space
+typedef struct Point{
+    float x;
+    float y;
+} point_t;
+
+typedef struct Line{
+    point_t start;
+    point_t end;
+} line_t;
+
+typedef struct Rect{
+    point_t bottom_left;
+    float width;
+    float height;
+} rect_t;
+
+vector2_t computeForceForRect(rect_t rect, point_t end_effector_pos, vector2_t end_effector_vel){}
+
+vector2_t computeForceForLine(line_t line, point_t end_effector_pos, vector2_t end_effector_vel){}
